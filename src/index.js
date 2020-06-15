196ea3b2c9eb23dbd1dc8295db0ce5f953e2fcb9
xiaoquanquanxin <xxkl123@126.com>
2020-06-15 19:05:12 +0800

    删除多余

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-// import './index.css';
-
-// import { Game } from './components/game.jsx';
-// import { KnowLedge } from './components/knowLedge';
-//
-// //  功能
-// ReactDOM.render(
-//     <Game/>,
-//     document.getElementById('root'),
-// );
-//
-// //  笔记
-// ReactDOM.render(
-//     <KnowLedge i={new Date().toLocaleString()}/>,
-//     document.getElementById('log'),
-// );
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { remSet } from './utils/utils';
