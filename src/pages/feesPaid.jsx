37a22187fb54255ea2d02b8ad77cb970a3fda326
xiaoquanquanxin <xxkl123@126.com>
2020-06-16 18:32:16 +0800

    分footer

---
+++ src/pages/feesPaid.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../css/color.css';
+
+// 支付成功内容
+export class FeesPaid extends React.Component {
+    // constructor(props){
+    //     super(props);
+    //     // console.log(this.props.location);
+    // }
+
+    componentDidMount(){
+        //  请求
+
+    }
+
+    render(){
+        return (
+            <div className='basic-struct'>
+                费用支付
+            </div>
+        );
+    }
+}
---
+++ src/pages/orderConfirm.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../css/color.css';
+import { OrderDetailInfo } from '../components/orderDetial/orderDetailInfo';
+import { OrderDetailStatus } from '../components/orderDetial/orderDetailStatus';
+import { OrderDetailBasic } from '../components/orderDetial/orderDetailBasic';
+
+// 支付成功内容
+export class OrderConfirm extends React.Component {
+    // constructor(props){
+    //     super(props);
+    //     // console.log(this.props.location);
+    // }
+
+    componentDidMount(){
+        //  请求
+
+    }
+
+    render(){
+        return (
+            <div className='basic-struct'>
+                {/*订单状态*/}
+                <OrderDetailStatus/>
+                {/*订单基础信息*/}
+                <OrderDetailBasic/>
+                {/*信息*/}
+                <OrderDetailInfo
+                    code={122}
+                    amount={45232}
+                    completionTime={3223}
+                />
+            </div>
+        );
+    }
+}
