96fa22babfe9d454b5c9b26b52c7ecdba0395146
xiaoquanquanxin <xxkl123@126.com>
2020-06-17 15:28:37 +0800

    关注点分离

--- src/components/feesPaid/choosePaymentMethod.jsx
+++ src/components/feesPaid/choosePaymentMethod.jsx
@@ -2,30 +2,76 @@ import React from 'react';
 import './choosePaymentMethod.css';
 import { SubTitle } from '../subTitle/subTitle';
 import { PaymentMethods } from './paymentMethods';
+//  微信支付宝图图
+import logo_weixin from '../../images/logo_weixin.png';
+import logo_zhifubao from '../../images/logo_zhifubao.png';
 
 // 选择支付方式模块
 export function ChoosePaymentMethod(props){
-    //  支付方式
-    const list = [3, 4].map((item) => {
-        // console.log(`key:${item}`, props.checkedMethod);
+    //  微信和支付宝数据，将来来自props
+    const list = processingData([3, 4], props.checkedMethod);
+    const text = '选择支付方式';
+    return (
+        <div className='choose-payment-method'>
+            <SubTitle text={text}/>
+            <MethodList
+                list={list}
+                handleClickCheck={props.handleClickCheck}
+            />
+        </div>
+    );
+}
+
+//  支付方式
+function MethodList(props){
+    const list = props.list.map((item, index) => {
         return (
             <PaymentMethods
-                key={item}
-                //  支付方式
-                method={item}
-                //  勾选的支付方式
-                checkedMethod={props.checkedMethod}
+                //  key
+                key={index}
+                //  数据
+                img={item.img}
+                alt={item.alt}
+                text={item.text}
+                method={item.method}
+                basicClassName={item.basicClassName}
+                extraClassName={item.extraClassName}
                 //  事件
                 handleClickCheck={props.handleClickCheck}
             />
         );
     });
     return (
-        <div className='choose-payment-method'>
-            <SubTitle text='选择支付方式'/>
-            <ul className='choose-payment-method-list'>
-                {list}
-            </ul>
-        </div>
+        <ul className='choose-payment-method-list'>
+            {list}
+        </ul>
     );
+}
+
+//  处理数据
+function processingData(list, checkedMethod){
+    return list.map((method) => {
+        const state = {
+            method,
+        };
+        switch (method) {
+            case 3:
+                state.text = '微信支付';
+                state.alt = '微信支付';
+                state.img = logo_weixin;
+                break;
+            case 4:
+                state.text = '支付宝支付';
+                state.alt = '支付宝支付';
+                state.img = logo_zhifubao;
+                break;
+            default:
+                throw new Error(method);
+        }
+        //  基础class
+        state.basicClassName = 'weui-icon-checkedbox';
+        //  勾选状态
+        state.extraClassName = checkedMethod === method ? ' weui-icon-success' : ' weui-icon-freeze';
+        return state;
+    });
 }
--- src/components/feesPaid/paymentMethods.jsx
+++ src/components/feesPaid/paymentMethods.jsx
@@ -2,44 +2,20 @@ import React from 'react';
 import './paymentMethods.css';
 import '../../css/color.css';
 
-//  微信支付宝图图
-import logo_weixin from '../../images/logo_weixin.png';
-import logo_zhifubao from '../../images/logo_zhifubao.png';
-
 //  实际支付方式
 export function PaymentMethods(props){
-    //  存储
-    const state = {};
-    switch (props.method) {
-        case 3:
-            state.text = '微信支付';
-            state.alt = '微信支付';
-            state.img = logo_weixin;
-            break;
-        case 4:
-            state.text = '支付宝支付';
-            state.alt = '支付宝支付';
-            state.img = logo_zhifubao;
-            break;
-        default:
-            throw new Error(props.method);
-    }
-    //  基础class
-    const basicClassName = 'weui-icon-checkedbox';
-    //  勾选状态
-    const extraClassName = props.checkedMethod === props.method ? ' weui-icon-success' : ' weui-icon-freeze';
     return (
         <li className='choose-payment-method-item border-grey'>
             <div className='logo'>
                 <img className='logo-image'
-                     src={state.img}
-                     alt={state.alt}/>
-                <span>{state.text}</span>
+                     src={props.img}
+                     alt={props.alt}/>
+                <span>{props.text}</span>
             </div>
             <div className='weui-icon-wrap'
                  onClick={() => (props.handleClickCheck(props.method))}
             >
-                <div className={basicClassName + extraClassName}/>
+                <div className={props.basicClassName + props.extraClassName}/>
             </div>
         </li>
     );
